0895cc489b77ef56ae30f3f7bf4fe9eabe77f42a
nwabekeyi <chidi90simeon@gmail.com>
2024-03-21 18:50:01 +0100

    commitfor homepage style

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,71 @@
-function Main (){
-    return(
-<div></div>
-    )
+import React from 'react';
+import '../styles/main.css';
+import restauranfood from '../assets/restauranfood.jpg';
+import greekSalad from '../assets/greekSalad.jpg';
+import Bruchetta from '../assets/Bruchetta.png';
+import lemonDessert from '../assets/lemonDessert.jpg';
+
+function Main() {
+    const menuData = [
+        {
+            image: greekSalad,
+            name: "Greek Salad",
+            price: "N5,500",
+            description: "The famous Greek salad of crispy lettuce, peppers, olives, our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons"
+        },
+        {
+            image: Bruchetta,
+            name: "Bruchetta",
+            price: "N5,500",
+            description: "The famous bruschetta of crispy lettuce, peppers, olives, our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons"
+        },
+        {
+            image: lemonDessert,
+            name: "Lemon Dessert",
+            price: "N5,500",
+            description: "The famous lemon dessert of crispy lettuce, peppers, olives, our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons"
+        }
+    ];
+
+    return (
+        <>
+            <section className="intro">
+                <div className='intro-container'>
+                    <article className="page-intro">
+                        <h1>Little Lemon</h1>
+                        <h2>Chicago</h2>
+                        <h3>We are a family owned Mediterranean restaurant. Focused on traditional recipes served with a modern twist.</h3>
+                        <button className='reserve-table'>Reserve a table</button>
+                    </article>
+                    <article>
+                        <img src={restauranfood} alt="a chef carrying a tray of food" className="intro-image" />
+                    </article>
+                </div>
+            </section>
+            <section className="week-special">
+                <article className='special-order'>
+                    <h1 className='week'>This week's Specials!</h1>
+                    <button className='order-online'>Online Menu</button>
+                </article>
+                <article className="menu" style={{ display: "flex" }}>
+                    {menuData.map((item, index) => (
+                        <div className="menu-item" key={index}>
+                            <img src={item.image} alt={item.name} className='menu-image' />
+                            <div className="details">
+                                <div className='name-price'>
+                                    <h2>{item.name}</h2>
+                                    <p>{item.price}</p>
+                                </div>
+                                <p className='menu-description'>{item.description}</p>
+                            </div>
+                        </div>
+                    ))}
+                </article>
+            </section>
+            <section className="testimonial"></section>
+            <section className="about"></section>
+        </>
+    );
 }
 
-export default Main
+export default Main;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,18 +1,19 @@
 import Logo from '../assets/Logo.png';
+import '../styles/navbar.css'
 
 
 
 function Nav() {
     return(
         <nav>
-            <img src={Logo} alt="company logo"/>
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Menu</li>
-                <li>Reservations</li>
-                <li>Order Online</li>
-                <li>Login</li>
+            <img src={Logo} alt="company logo" className='logo'/>
+            <ul id='nav-link'>
+                <li className='nav-links'>Home</li>
+                <li className='nav-links'>About</li>
+                <li className='nav-links'>Menu</li>
+                <li className='nav-links'>Reservations</li>
+                <li className='nav-links'>Order Online</li>
+                <li className='nav-links'>Login</li>
             </ul>
         </nav>
     )
